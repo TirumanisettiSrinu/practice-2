c0f69ad2d46b41a6bfab984b04d2203bcf98c503
Tirumanisetti Srinu <srinu.t22@iiits.in>
2025-02-26 18:21:05 +0530

    Designing congrats card

---
+++ index.js
@@ -0,0 +1,23 @@
+const element = (
+    // Write your code here.
+    <div class="container">
+      <h1>Congratulations</h1>
+      <div class="card">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+          alt="Person"
+          class="profile"
+        />
+        <h2>Kiran V</h2>
+        <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+          alt="Watch"
+          class="watch"
+        />
+      </div>
+    </div>
+  );
+  
+  ReactDOM.render(element, document.getElementById("root"));
+  
